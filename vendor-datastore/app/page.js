46f5031319d5003084b361c044d8836a423e1db9
Nishan9t <pandeynisha9t@gmail.com>
2023-10-07 12:18:22 +0530

    project initialized

---
+++ vendor-datastore/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='max-w-screen h-screen w-full mx-auto py-16 px-4 mx-auto items-center text-center'>
+      hello
+    </div>
+  )
+}
