faa6fbc28e9a3613131e2c7a2232a9cf314e5841
Nishan9t <pandeynisha9t@gmail.com>
2023-10-09 12:28:04 +0530

    input data component is created

--- vendor-datastore/components/InputData.jsx
+++ vendor-datastore/components/InputData.jsx
@@ -1,9 +1,133 @@
 'use client'
 
+import { useState } from "react";
+
 export default function InputData() {
+  const [formData, setFormData] = useState({
+    vendorName: '',
+    bankAccountno: '',
+    bankName: '',
+    addressLine1: '',
+    addressLine2: '',
+    city: '',
+    country: '',
+    zipcode: ''
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log(formData);
+  };
+
   return (
-    <div>
+    <div className="container mx-auto p-4">
+      <h2 className="text-2xl font-semibold mb-4">Vendor Information</h2>
+      <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Vendor Name</label>
+          <input
+            type="text"
+            name="vendorName"
+            value={formData.vendorName}
+            onChange={handleInputChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
+
+
+          <label className="block text-sm font-medium text-gray-600">Bank Account Number</label>
+          <input
+            type="text"
+            name="bankaccountno"
+            value={formData.bankAccountno}
+            onChange={handleInputChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
+
+
+           <label className="block text-sm font-medium text-gray-600">Bank Name</label>
+          <input
+            type="text"
+            name="bankName"
+            value={formData.bankName}
+            onChange={handleInputChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
+
+
+          <label className="block text-sm font-medium text-gray-600">Address Line1</label>
+          <input
+            type="text"
+            name="addressLine1"
+            value={formData.addressLine1}
+            onChange={handleInputChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
+
+
+          <label className="block text-sm font-medium text-gray-600">Address Line2</label>
+          <input
+            type="text"
+            name="addressLine2"
+            value={formData.addressLine2}
+            onChange={handleInputChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            
+          />
+
+
+          <label className="block text-sm font-medium text-gray-600">City</label>
+          <input
+            type="text"
+            name="city"
+            value={formData.city}
+            onChange={handleInputChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
+
+
+          <label className="block text-sm font-medium text-gray-600">Country</label>
+          <input
+            type="text"
+            name="country"
+            value={formData.country}
+            onChange={handleInputChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
+
+
+
+          <label className="block text-sm font-medium text-gray-600">Zip Code</label>
+          <input
+            type="text"
+            name="zipcode"
+            value={formData.zipcode}
+            onChange={handleInputChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
         
+          
+        </div>
+        {/* Add similar input fields for other form fields */}
+        {/* ... */}
+        <div className="mt-4">
+          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
--- vendor-datastore/components/Vendor.jsx
+++ vendor-datastore/components/Vendor.jsx
@@ -2,23 +2,41 @@
 
 
 
+import { useState } from 'react';
 import SinginBtn from './SinginBtn'
 import { useSession } from 'next-auth/react'
+import InputData from './InputData';
+
 
 
 export default function Vendor() {
 
+    const [create,setCreate]=useState(false)
     const {status, data:session} = useSession();
 
     if(status==='authenticated')
     {
-        return <div className='flex p-4 w-full border-2 py-4 items-center justify-center'>
-            <div className='text-center  my-auto border-2 rounded-full bg-green-700 text-xl font-bold px-3 py-1'>{session?.user?.name.charAt(0)}</div>
-            <div className='ml-4'>
-                <div className=' font-bold text-green-700'>{session?.user?.name}</div>
-                <div className=' font-bold  text-green-700'>{session?.user?.email}</div>
-            </div>
+        return (
+            <div className='w-full'>
+                <div className='flex p-4 w-full border-2 py-4 items-center justify-center'>
+                    <div className='text-center  my-auto border-2 rounded-full bg-green-700 text-xl font-bold px-3 py-1'>{session?.user?.name.charAt(0)}</div>
+                    <div className='ml-4'>
+                        <div className=' font-bold text-green-700'>{session?.user?.name}</div>
+                        <div className=' font-bold  text-green-700'>{session?.user?.email}</div>
+                    </div>
+                
+                </div>
+                <div className='mx-auto text-center mt-16'>
+                {
+                    !create?
+                    <button onClick={()=>setCreate(true)} className='bg-green-500 text-white px-4 py-3'>Create Vendor</button>
+                    :
+                    <InputData/>
+                }
+                    
+                </div>
         </div>
+        );
     }
     else
     {
